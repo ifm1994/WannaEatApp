67d229c36ab6829ec7007177732b57684f9bd7d6
Sakro <sakro.developer@gmail.com>
2018-04-04 00:06:25 +0200

    Conectada y registrada para usar la api de google maps

--- app/src/main/java/com/example/ifmfo/wannaeatapp/HomeActivity.java
+++ app/src/main/java/com/example/ifmfo/wannaeatapp/HomeActivity.java
@@ -1,13 +1,26 @@
 package com.example.ifmfo.wannaeatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private CardView searchRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        searchRestaurantsButton = (CardView) findViewById (R.id.searchRestaurantButton);
+        searchRestaurantsButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View arg0){
+                Intent intent = new Intent(HomeActivity.this, findRestaurantActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
